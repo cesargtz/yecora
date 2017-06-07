388d9c0a0ae4ac1e5651a1a69ac9175842fc7bfc
César Gtz <cesareomargtz@gmail.com>
2017-06-07 10:08:54 -0600

    virtualEnv Django yecora

---
+++ yecora/yecora/settings/prod.py
@@ -0,0 +1,16 @@
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STATIC_URL = '/static/'
